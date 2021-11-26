ad3690b7068ea1f3a03e74e57fa76575467b3e9a
Bartekkom <bartek.kom524@gmail.com>
2021-11-26 17:29:57 +0100

    First commit

---
+++ src/GamePanel.java
@@ -0,0 +1,30 @@
+import javax.swing.JPanel;
+
+import java.awt.Graphics;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class GamePanel extends JPanel implements ActionListener{
+
+	GamePanel(){
+		
+	}
+	
+	public void startGame() {
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+	}
+	
+	public void move() {
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
